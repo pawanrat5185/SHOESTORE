942996f2b6551abafd4e2625edebf63cd92bf541
pawanrat5185 <min167002@gmail.com>
2022-07-10 18:16:39 +0700

    <SHOESTORE_1>

---
+++ lib/pages/home/home_bottom_nav_bar.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shoestore/themes/color.dart';
+
+class HomeBottomNavBar extends StatelessWidget {
+  const HomeBottomNavBar({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 65,
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        color: darkColor,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(25),
+          topRight: Radius.circular(25),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon( 
+            Icons.category_outlined,
+            color: whiteColor,
+            size: 32,
+          ),
+          Icon( 
+            CupertinoIcons.cart_fill,
+            color: whiteColor,
+            size: 32,
+          ),
+          Icon( 
+            Icons.favorite_border,
+            color: whiteColor,
+            size: 32,
+          ),
+          Icon( 
+            Icons.person,
+            color: whiteColor,
+            size: 32,
+          ),
+        ],
+      ),
+    );
+  }
+}
